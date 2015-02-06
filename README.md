dc336569251287f9061ebe6f2127f6d37d456c9a
Emil Öberg <emil.oberg@monator.com>
2015-02-06 13:00:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# node-red-contrib-telldus
+
+# WIP, Work In Progress!
+
+**This Node-RED node will send and receive data to/from a Tellstick, Tellstick Duo, and Tellstick Net**
---
+++ telldus/telldus.js
@@ -0,0 +1,100 @@
+/**
+ * Copyright 2013,2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+	"use strict";
+
+
+
+
+
+
+
+	/**
+	 * Telldus Config
+	 *
+	 */
+	function TelldusInputsConfigNode(n) {
+		RED.nodes.createNode(this,n);
+		this.name = n.name;
+	}
+	RED.nodes.registerType("telldus-inputs-config", TelldusInputsConfigNode);
+
+	/**
+	 * Telldus In
+	 *
+	 */
+	function  TelldusInNode(n) {
+		RED.nodes.createNode(this,n);
+		this.name = n.name;
+		this.inputconfig = n.inputconfig;
+	}
+	RED.nodes.registerType("telldus-in", TelldusInNode);
+
+	/**
+	 *
+	 * Telldus OUT
+	 *
+	 */
+	function TelldusOutNode(n) {
+		RED.nodes.createNode(this,n);
+		this.name = n.name;
+		this.device = n.device || "";
+		this.method = n.method || "";
+		this.dimvalue = n.dimvalue || "";
+		this.devicefriendlyname = n.devicefriendlyname || "";
+	}
+	RED.nodes.registerType("telldus-out",TelldusOutNode);
+
+
+	// Kolla hardware/35-arduino.js
+	RED.httpAdmin.get("/telldus/devices",function(req,res) {
+		res.writeHead(200, {'Content-Type': 'application/json'});
+
+
+		var telldus = require('telldus');
+		telldus.getDevices(function(err, data) {
+			if (err) {
+				// Todo, make this into Node-RED error. How do you do that?
+				console.log('Telldus Error: ');
+				console.log(require('util').inspect(err, { showHidden: true, depth: null, colors: true }));
+			} else {
+
+				// Only get Devices
+				data = data.filter(function (device) {
+					return device.type === 'DEVICE';
+				});
+
+				// Convert method array into properties object for better API.
+				var methods;
+				for (var i = 0; i < data.length; i++) {
+					methods = {};
+					data[i].methods.forEach(function (method) {
+						methods[method.toLowerCase()] = true;
+					});
+					data[i].methods = methods;
+				}
+
+				res.write(JSON.stringify(data));
+				res.end();
+			}
+		});
+
+
+
+	});
+
+};
