7b1a6de115eeca52eb80c0699b420549fbf12f36
Emil Öberg <emil.oberg@monator.com>
2015-02-28 18:43:11 +0100

    Refactoring: Split all modules into shorter separate files

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,15 +5,46 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var debug	= require('gulp-debug');
- 
+var fileinclude = require('gulp-file-include');
+var rename = require('gulp-rename');
+var watch = require('gulp-watch');
+var runSequence = require('run-sequence');
+
 gulp.task('lint', function () {
-    return gulp.src(['telldus/*.js', 'telldus/lib/*.js'])
+    return gulp.src(['telldus/*.js', 'telldus/lib/*.js', 'telldus/*.html', 'telldus/lib/*.html'])
         .pipe(debug())
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failOnError());
 });
- 
-gulp.task('default', ['lint'], function () {
-    // This will only run if the lint task is successful... 
+
+
+gulp.task('merge-files', function() {
+	gulp.src(['lib/*.js-template'])
+		.pipe(fileinclude({
+			prefix: '@@',
+			basepath: '@file'
+		}))
+		.pipe(rename(function (path) {
+			path.extname = '.js';
+		}))
+		.pipe(gulp.dest('./telldus/'));
+
+	gulp.src(['lib/*.html-template'])
+		.pipe(fileinclude({
+			prefix: '@@',
+			basepath: '@file'
+		}))
+		.pipe(rename(function (path) {
+			path.extname = '.html';
+		}))
+		.pipe(gulp.dest('./telldus/'));
 });
+
+gulp.task('watch-files', function() {
+	watch(['lib/*.js-include', 'lib/**/*.js-include', 'lib/*.html', 'lib/**/*.html'], function() {
+		runSequence('merge-files');
+	});
+});
+
+gulp.task('default', ['watch-files']);
