60c5b46b49b50ce47062722f40707f2a99d0f554
Emil Öberg <emil.oberg@monator.com>
2015-03-02 12:52:19 +0100

    added api to get alla data from a single device

--- telldus/lib/telldusDeviceTypes.js
+++ telldus/lib/telldusDeviceTypes.js
@@ -1,45 +1,8 @@
 'use strict';
 
 
-
+var telldus = require('telldus');
 var deviceTypes = require('./../resources/deviceTypesConfigurations.json');
-
-//(function() {
-//	var executed = false;
-//	return function () {
-//		if (!executed) {
-//			executed = true;
-//			deviceTypes = require('./../resources/deviceTypes.json');
-//			deviceTypes = deviceTypes.sort(function (a, b) {
-//				if (a.make > b.make) {
-//					return 1;
-//				} else if (a.make < b.make) {
-//					return -1;
-//				} else {
-//					if (a.name > b.name) {
-//						return 1;
-//					} else if (a.name < b.name) {
-//						return -1;
-//					} else {
-//						return 0;
-//					}
-//				}
-//			});
-//
-//		}
-//	};
-//})();
-//
-//
-//function get() {
-//	return deviceTypes;
-//}
-
-//function getProtocols() {
-//	return Object.keys(deviceTypes).map(function (protocol) {
-//		return protocol;
-//	});
-//}
 
 function getBrands() {
 	var brands = {};
@@ -74,21 +37,145 @@ function getModels(brandName) {
 	return models;
 }
 
-//function getParameters(modelName) {
-//	var ret = {};
-//	Object.keys(deviceTypes).forEach(function (protocol) {
-//		Object.keys(deviceTypes[protocol]).forEach(function (type) {
-//			deviceTypes[protocol][type].devices.forEach(function (device) {
-//				if (device.model === modelName) {
-//					ret = deviceTypes[protocol][type].parameters;
-//				}
-//			});
-//		});
-//	});
-//	return ret;
-//}
+function getParametersName(modelName) {
+	var ret = {};
+	Object.keys(deviceTypes).forEach(function (protocol) {
+		Object.keys(deviceTypes[protocol]).forEach(function (type) {
+			deviceTypes[protocol][type].devices.forEach(function (device) {
+				if (device.model === modelName) {
+					ret = deviceTypes[protocol][type].parameters;
+				}
+			});
+		});
+	});
+	return ret;
+}
 
-//module.exports.getProtocols = getProtocols;
+function getDevice(deviceId) {
+
+	var out = {
+		id: deviceId,
+		methods: {},
+		model: '',
+		name: '',
+		protocol: '',
+		status: {},
+		type: ''
+	};
+
+	return out;
+}
+
+function getDevices(deviceId, cb) {
+	// todo validate deviceId
+	if(!cb) {
+		cb = deviceId;
+		deviceId = -1;
+	}
+
+	telldus.getDevices(function(err, data) {
+		if (err) {
+			cb(err);
+		} else {
+
+			// If requested with a DeviceId, only return that device.
+			// Unsure if there's anything else than 'devices' but lets filter
+			// them anyways.
+			data = data.filter(function (device) {
+				if (deviceId > 0) {
+					return device.id === deviceId && device.type === 'DEVICE';
+				}
+				return device.type === 'DEVICE';
+			});
+
+			// Sort
+			data.sort(function (a, b) {
+				if (a.name > b.name) {
+					return 1;
+				}
+				if (a.name < b.name) {
+					return -1;
+				}
+				return 0;
+			});
+
+			// Convert method array into properties object for better API.
+			var methods;
+			for (var i = 0; i < data.length; i++) {
+				methods = {};
+				/*eslint-disable no-loop-func */
+				data[i].methods.forEach(function (method) {
+					methods[method.toLowerCase()] = true;
+				});
+				/*eslint-enable no-loop-func */
+				data[i].methods = methods;
+			}
+
+			// If asked for a single device, make sure to return an object.
+			if (deviceId > 0) {
+				if (data.length === 1) {
+					data = data[0];
+				} else {
+					cb(deviceId + ' did not resolve to a single device');
+				}
+			}
+
+			cb(null, data);
+
+		}
+	});
+}
+
+function getParametersValues(deviceId, cb) {
+
+	var when = require('when');
+	var out = {};
+
+	telldus.getModel(deviceId, function (err, model) {
+		if (err) {
+			cb(err);
+			return;
+		}
+
+		var parametersName = Object.keys(getParametersName(model)).map(function (param) {
+			return param;
+		});
+
+		var arrPromises = parametersName.map(function (method) {
+			return when.promise(function(resolve, reject) {
+				telldus.getDeviceParameter(deviceId, method, '', function(err2, value) {
+					if (err2) {
+						reject(err2);
+					} else {
+						resolve({
+							method: method,
+							value: value
+						});
+					}
+				});
+			});
+		});
+
+		when.settle(arrPromises).then(function(descriptors) {
+			var hasErrors = false;
+			descriptors.forEach(function (d) {
+				if (d.state === 'fulfilled') {
+					out[d.value.method] = d.value.value;
+				} else {
+					hasErrors = true;
+				}
+			});
+			if (hasErrors) {
+				cb('Could not find value for parameters');
+			}
+			cb(null, out);
+		});
+
+	});
+}
+
+module.exports.getDevice = getDevice;
+module.exports.getDevices = getDevices;
 module.exports.getBrands = getBrands;
 module.exports.getModels = getModels;
-//module.exports.getParameters = getParameters;
+module.exports.getParametersValues = getParametersValues;
